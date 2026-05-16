7d240030f8cd77cf3006a3780166adece844c86b
Gerhana0 <faadhilah0627@gmail.com>
2026-05-16 22:58:03 +0700

    Add, setting, and make authentification for register (frontend & backend)

--- src/web/app/Http/Controllers/AuthController.php
+++ src/web/app/Http/Controllers/AuthController.php
@@ -3,8 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\user;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
-class AuthController extends Controller
+class authController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'nim' => 'required|exists:master_mahasiswa_fatisda,nim|unique:users,nim',
+            'username' => 'required|unique:users,username',
+            'email_user' => 'required|email|unique:users,email_user',
+            'password' => 'required|min:8',
+        ], [
+            'nim.required' => 'NIM wajib diisi.',
+            'nim.exists' => 'NIM tidak ditemukan dalam data mahasiswa.',
+            'nim.unique' => 'NIM sudah terdaftar.',
+        ]);
+
+        user::create([
+            'nim' => $request->nim,
+            'username' => $request->username,
+            'email_user' => $request->email_user,
+            'password' => Hash::make($request->password),
+            'role' => 'user',
+        ]);
+
+        return redirect()->route('login')->with('success', 'Registrasi berhasil. Silakan login.');
+    }
+
+    public function login(Request $request)
+    {
+
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/login')->with('success', 'Kamu telah berhasil logout.');
+
+    }
 }
--- src/web/routes/web.php
+++ src/web/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\authController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,7 @@ Route::get('/', function () {
 Route::get('/register', function () {
     return view('register');
 })->name('register');
+
+Route::post('/login', [authController::class, 'login'])->name('login.proses');
+Route::post('/register', [authController::class, 'register'])->name('register.proses');
+Route::post('/logout', [authController::class, 'logout'])->name('logout');
