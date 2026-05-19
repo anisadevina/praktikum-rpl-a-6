f1e451910768aa6d667dc859e1dbda9ddacf0e95
Gerhana0 <faadhilah0627@gmail.com>
2026-05-19 21:12:56 +0700

    add feature Terakhir dilihat with data dummy

--- src/web/database/seeders/DatabaseSeeder.php
+++ src/web/database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             MataKuliahSeeder::class,
+            RiwayatAksesSeeder::class,
         ]);
     }
 }
--- src/web/routes/web.php
+++ src/web/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\authController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('login');
@@ -16,5 +17,32 @@ Route::post('/register', [authController::class, 'register'])->name('register.pr
 Route::post('/logout', [authController::class, 'logout'])->name('logout');
 
 Route::get('/beranda', function () {
-    return view('beranda', ['user' => auth()->user()]);
+    // return view('beranda', ['user' => auth()->user()]);
+    $user = auth()->user();
+
+    // ambil 4 mata kuliah terakhir yang diakses oleh user
+    $mataKuliahTerakhir = DB::table('riwayat_akses')
+        ->join('mata_kuliah', 'riwayat_akses.id_matkul', '=', 'mata_kuliah.id_matkul')
+        ->where('riwayat_akses.id_user', $user->id_user)
+        ->select('mata_kuliah.*')
+        ->orderBy('riwayat_akses.waktu_akses', 'desc')
+        ->limit(4)
+        ->get()
+        ->map(function ($item) {
+            $item->arsip = DB::table('dokumen')->where('id_matkul', $item->id_matkul)->count();
+            return $item;
+        });
+
+    $forumTerbaru = DB::table('forum_topik')
+        ->join('users', 'forum_topik.id_user', '=', 'users.id_user')
+        ->select('forum_topik.*', 'users.username')
+        ->orderBy('forum_topik.waktu_topik', 'desc')
+        ->limit(4)
+        ->get();
+
+    return view('beranda', [
+        'user' => $user,
+        'mataKuliahTerakhir' => $mataKuliahTerakhir,
+        'forumTerbaru' => $forumTerbaru,
+    ]);
 })->name('beranda')->middleware('auth');
