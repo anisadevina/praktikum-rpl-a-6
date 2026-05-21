14817cbfda657ec6cd7f666eba5e376edd6379a5
Anisa Devina <anisa.devina07@gmail.com>
2026-05-21 11:27:45 +0700

    feat: update search to redirect to matkul page

--- src/web/app/Http/Controllers/AuthController.php
+++ src/web/app/Http/Controllers/AuthController.php
@@ -105,7 +105,12 @@ class authController extends Controller
     public function matkul(Request $request)
     {
         $user = auth()->user();
+        $query = $request->input('q', '');
+
         $semuaMatkul = DB::table('mata_kuliah')
+            ->when($query, function ($q) use ($query) {
+                $q->where('nama_matkul', 'like', '%' . $query . '%');
+            })
             ->get()
             ->map(function ($item) {
                 $item->arsip = DB::table('dokumen')->where('id_matkul', $item->id_matkul)->count();
@@ -115,6 +120,7 @@ class authController extends Controller
         return view('matkul', [
             'user' => $user,
             'semuaMatkul' => $semuaMatkul,
+            'query' => $query,
         ]);
     }
 
