73668422adc893513700a2b2ba3af7799184d4bf
Gerhana0 <faadhilah0627@gmail.com>
2026-05-25 15:12:09 +0700

    Resolve Error logout

--- src/web/app/Http/Controllers/AuthController.php
+++ src/web/app/Http/Controllers/AuthController.php
@@ -102,15 +102,6 @@ class authController extends Controller
         return response()->json($matkul);
     }
 
-    public function logout(Request $request)
-    {
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        return redirect('/')->with('success', 'Kamu telah berhasil logout.');
-
-    }
-
     public function matkul(Request $request)
     {
         $user = auth()->user();
