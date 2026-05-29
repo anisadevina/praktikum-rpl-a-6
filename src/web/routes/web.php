edd0462bc5fb33ef383650bdd716cf3ff5275647
Jelita Kustyara <jelitakustyara06@gmail.com>
2026-05-29 13:36:09 +0700

    feat: add page arsip

--- src/web/routes/web.php
+++ src/web/routes/web.php
@@ -13,9 +13,14 @@ Route::get('/register', function () {
     return view('register');
 })->name('register');
 
+Route::get('/arsip', function () { // dari jeje
+    return view('arsip');
+})->name('arsip');
+
 Route::post('/login', [AuthController::class, 'login'])->name('login.proses');
 Route::post('/register', [AuthController::class, 'register'])->name('register.proses');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
 
 // Kelompokkan rute yang butuh middleware 'auth'
 Route::middleware('auth')->group(function () {
@@ -24,4 +29,5 @@ Route::middleware('auth')->group(function () {
     Route::get('/search', [MatkulController::class, 'search'])->name('search');
     Route::get('/matkul', [MatkulController::class, 'index'])->name('matkul');
     Route::get('/matkul/detail', [MatkulController::class, 'detail'])->name('matkul.detail');
+    Route::get('/arsip', function () { return view('arsip'); })->name('arsip'); // dari jeje
 });
