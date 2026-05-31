4f1a85a6bbdba81bb6d5afc3d1f33dd483ef82ab
Gerhana0 <faadhilah0627@gmail.com>
2026-05-31 21:48:38 +0700

    fix inconsistent code

--- src/web/routes/web.php
+++ src/web/routes/web.php
@@ -28,7 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/beranda/data', [BerandaController::class, 'getData'])->name('beranda.data');
 
     // Matkul
-    Route::get('/search', [MatkulController::class, 'search'])->name('search');
     Route::get('/matkul', [MatkulController::class, 'index'])->name('matkul');
     Route::get('/matkul/data', [MatkulController::class, 'getIndexData'])->name('matkul.data');
     Route::get('/matkul/detail', [MatkulController::class, 'detail'])->name('matkul.detail');
